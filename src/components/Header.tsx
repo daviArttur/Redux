eda5854b1b1dfa8388904c3b8856fd246c7300c5
Davi Artur <daviartur2004@gmail.com>
2022-08-06 12:55:59 -0300

    (feat) esconder botão de deslogar usuário, caso não tenha sido feito login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,15 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootStateType } from '../store/configureStore'
 import getUser from '../store/getUser'
-
 import openModal from '../store/openModal' 
 
 // Styles 
 import styles from './Header.module.css'
 
 const Header = () => {
-
+  const userDataLogin = useSelector((state : RootStateType) => state.user.data)
   const dispatch = useDispatch()
-
+  
   function handleClickDisconectUser() {
     window.localStorage.removeItem('token')
     dispatch(openModal.actions.setOpenModal('components/Form'))
@@ -19,7 +19,13 @@ const Header = () => {
   return (
     <header>
       <h1 className={styles.title}>Mini Dogs</h1>
-      <button className={styles.button} onClick={handleClickDisconectUser}> </button>
+      {userDataLogin && (
+        <button 
+        className={styles.button} 
+        onClick={handleClickDisconectUser}
+        data-testid="button"
+      > </button>
+      )} 
     </header>
   )
 }
